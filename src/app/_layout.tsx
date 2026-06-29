f3f154f85a56b08053c1054c0de5cd11c8ae7287
Eduard <reegankens@gmail.com>
2026-06-29 15:58:19 +0700

    add nativewind - tailwindcss

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,6 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from 'expo-router';
 import { useColorScheme } from 'react-native';
+import "../../global.css"; //<- import your css file
 
 import { AnimatedSplashOverlay } from '@/components/animated-icon';
 import AppTabs from '@/components/app-tabs';
