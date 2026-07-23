d0dfdd62fc2cefe18232cb73553bdebc8ffc7621
Eduard <reegankens@gmail.com>
2026-07-23 11:09:55 +0700

    fix reference error message event doesn't exist
    Fix ReferenceError: Property 'MessageEvent' doesn't exist

--- msw.polyfills.ts
+++ msw.polyfills.ts
@@ -1,4 +1,19 @@
-import "./polyfills/web-events";
 import "fast-text-encoding";
 import "react-native-url-polyfill/auto";
 
+//ReferenceError: Property 'MessageEvent' doesn't exist
+//https://github.com/mswjs/mswjs.io/issues/453
+function defineMockGlobal(name) {
+  if (typeof global[name] === "undefined") {
+    global[name] = class {
+      constructor(type, eventInitDict) {
+        this.type = type;
+        Object.assign(this, eventInitDict);
+      }
+    };
+  }
+}
+
+["MessageEvent", "Event", "EventTarget", "BroadcastChannel"].forEach(
+  defineMockGlobal,
+);
