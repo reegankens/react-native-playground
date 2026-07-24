a3274b7db47c90b97436c84b4ad4c7a6d410cbd3
Eduard <reegankens@gmail.com>
2026-07-24 10:23:03 +0700

    parse json request body in msw post handler

--- mocks/handlers.ts
+++ mocks/handlers.ts
@@ -1,6 +1,12 @@
 // src/mocks/handlers.ts
 import { http, HttpResponse } from "msw";
 
+type FormPayload = {
+  id?: string;
+  fullName: string;
+  email: string;
+};
+
 export const handlers = [
   http.get("http://api.localhost:8081/register", () => {
     return HttpResponse.json({
@@ -9,11 +15,21 @@ export const handlers = [
       email: "bhakti@eduardd.com",
     });
   }),
-  http.post("http://api.localhost:8081/register", () => {
-    return HttpResponse.json({
-      id: "abc-123",
-      fullName: "Eduard",
-      email: "bhakti@eduardd.com",
-    });
+  http.post("http://api.localhost:8081/register", async ({ request }) => {
+    try {
+      console.log("handler masuk");
+      const payloadObj = (await request.json()) as FormPayload;
+      // const text = await request.text();
+      // console.log("body =", payloadObj);
+
+      return HttpResponse.json({
+        id: "1~uuid",
+        fullName: payloadObj.fullName,
+        email: payloadObj.email,
+      });
+    } catch (e) {
+      console.error("HANDLER ERROR", e);
+      throw e;
+    }
   }),
 ];
